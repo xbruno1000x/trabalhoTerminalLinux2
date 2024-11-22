fe388f1b221a3497e55215514654bf160035f0e9
xbruno1000x <88844126+xbruno1000x@users.noreply.github.com>
2024-11-22 15:35:26 -0300

    Update trabalho.sh

--- trabalho.sh
+++ trabalho.sh
@@ -1,1 +1,44 @@
 #!/bin/bash
+
+# 1.
+sudo apt install -y fortune
+
+# 2.
+fortune > mensagem1.txt
+fortune > mensagem2.txt
+fortune > mensagem3.txt
+
+# Mostrar o conteúdo dos arquivos no terminal
+echo "Conteúdo de mensagem1.txt:"
+cat mensagem1.txt
+echo "Conteúdo de mensagem2.txt:"
+cat mensagem2.txt
+echo "Conteúdo de mensagem3.txt:"
+cat mensagem3.txt
+
+# 3. 
+cat mensagem1.txt mensagem2.txt mensagem3.txt > mensagens.txt
+echo "Conteúdo final do arquivo mensagens.txt:"
+cat mensagens.txt
+
+# 4. 
+cp mensagens.txt mensagens_antiga.txt
+fortune >> mensagens.txt
+
+# 5.
+wc -l < mensagens.txt > linhas.txt
+
+# 6. 
+egrep -o "a" mensagens.txt | wc -l > letras.txt
+
+# 7. 
+ls -R /etc > saida.txt 2> erros.txt
+
+# 8. 
+ls -R /etc &> saida_com_erros.txt
+
+# 9. 
+cut -d':' -f1 /etc/passwd > usuarios.txt
+
+# 10. 
+sort usuarios.txt > usuarios_ordenados.txt
